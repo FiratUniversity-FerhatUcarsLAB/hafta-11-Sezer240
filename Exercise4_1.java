ac4b9762cbf08d8ab9ef8d9240b0e3746c4f3038
Sezer <240541031@firat.edu.tr>
2025-12-04 18:34:12 +0300

    Refactor input handling for date and month

--- Exercise4_1.java
+++ Exercise4_1.java
@@ -10,21 +10,49 @@ import java.util.Scanner;
     }
 
     public static void main(String[] args) {
-        Scanner input=new Scanner(System.in);
-
+       Scanner input=new Scanner(System.in);
+        
        System.out.print("Lutfen gunu giriniz(Carsamba, Cumartesi vb.): ");
        String day=input.next();
-       System.out.print("Lutfen ayin kaçinci gunu oldugunu giriniz(16,24 vb): ");
-       int date=input.nextInt();
-       System.out.print("Lutfen hangi ayda oldugunuzu giriniz(Mayis, Aralik vb.): ");
-       String month=input.next();
-       System.out.print("Lutfen hangi yilda oldugunuzu giriniz: ");
-       int year=input.nextInt();
+       if(day.equalsIgnoreCase("Pazartesi") ||
+          day.equalsIgnoreCase("Sali") ||
+          day.equalsIgnoreCase("Carsamba") ||
+          day.equalsIgnoreCase("Persembe") ||
+          day.equalsIgnoreCase("Cuma") ||
+          day.equalsIgnoreCase("Cumartesi") ||
+          day.equalsIgnoreCase("Pazar")){
 
-        
-        printAmerican(day, date, month, year);
-        printEuropean(day, date, month, year);
+            System.out.print("Lutfen ayin kaçinci gunu oldugunu giriniz(16,24 vb.): ");
+            int date=input.nextInt();
+            if(date >=1 && date <= 31){
+            System.out.print("Lutfen hangi ayda oldugunuzu giriniz(Mayis, Aralik vb.): ");
+            String month=input.next();
+            if(month.equalsIgnoreCase("Ocak") ||
+               month.equalsIgnoreCase("Subat") ||
+               month.equalsIgnoreCase("Mart") ||
+               month.equalsIgnoreCase("Nisan") ||
+               month.equalsIgnoreCase("Mayis") ||
+               month.equalsIgnoreCase("Haziran") ||
+               month.equalsIgnoreCase("Temmuz") ||
+               month.equalsIgnoreCase("Agustos") ||
+               month.equalsIgnoreCase("Eylul") ||
+               month.equalsIgnoreCase("Ekim") ||
+               month.equalsIgnoreCase("Kasim") ||
+               month.equalsIgnoreCase("Aralik")){
 
-        input.close();
+                System.out.print("Lutfen hangi yilda oldugunuzu giriniz: ");
+                int year=input.nextInt();
+
+                printAmerican(day, date, month, year);
+                printEuropean(day, date, month, year);
+            }else{
+                System.out.println("Turkce karakter kullanmayin.");
+            }
+            }else{
+                System.out.println("Ne zamandan beri bir ay 1-31 araliginin disinda :D");
+            }
+       }else{
+        System.out.println("Turkce karakter kullanmayin.");
+       } input.close();
     }
 } 
