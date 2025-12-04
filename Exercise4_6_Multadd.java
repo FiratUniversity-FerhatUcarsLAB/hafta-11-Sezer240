bdaa0864a5ae67fecf973f46207789ff4ef318dd
Sezer <240541031@firat.edu.tr>
2025-12-04 20:50:20 +0300

    Complete multadd and expSum methods with tests
    Implemented multadd and expSum methods, and added test cases in main.

--- Exercise4_6_Multadd.java
+++ Exercise4_6_Multadd.java
@@ -1,16 +1,18 @@
 public class Exercise4_6_Multadd {
 
     public static double multadd(double a, double b, double c) {
-        // TODO
-        return 0.0;
+        return a*b+c;
     }
 
     public static double expSum(double x) {
-        // TODO
-        return 0.0;
+        return multadd(x, Math.exp(-x), Math.sqrt(1 - Math.exp(-x)));
     }
 
     public static void main(String[] args) {
-        // TODO: test methods
+        System.out.println("sin(pi/4) + cos(pi/4)/2 = " + 
+            multadd(0.5, Math.cos(Math.PI / 4), Math.sin(Math.PI / 4)));
+        System.out.println("log(10) + log(20) = " + 
+            multadd(1.0, Math.log(10), Math.log(20)));
+        System.out.println("expSum(4.2) sonucu: " + expSum(4.2));
     }
-}
+} 
