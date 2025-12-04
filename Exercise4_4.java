af6d0ddcb459647b83e5ceee8f4b5c45291fd3fd
Sezer <240541031@firat.edu.tr>
2025-12-04 19:38:06 +0300

    Enhance comments on method usage and errors
    Added comments explaining compilation issues with void methods and return values.

--- Exercise4_4.java
+++ Exercise4_4.java
@@ -11,11 +11,17 @@ public class Exercise4_4 {
     public static void main(String[] args) {
 
         // 1) Donus degerini kullanmayin -> ne oluyor?
+        //program hata vermedi, çalıştı ama hiçbir çıktı vermedi
         getNumber();
 
         // 2) void metodu ifade icinde kullanin -> ne oluyor?
         // System.out.println(sayHello() + 7);
 
         // Cevaplarinizi yorum olarak ekleyin.
+        //java compile hatası verdi
+        /*Exception in thread "main" java.lang.Error: Unresolved compilation problem: 
+        The operator + is undefined for the argument type(s) void, int
+        at Exercise4_4.main(public class Exercise4_4.java:18)  
+        bu hata kodunda da derleme problemi oluştu, void ve int toplanamaz diyor*/
     }
-}
+} 
